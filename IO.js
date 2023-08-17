0884fc7293bd4b80609e76679523bd5826031369
haihaina <2415172072@qq.com>
2023-08-17 16:41:53 +0800

    feat:初始化

---
+++ IO.js
@@ -0,0 +1,264 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var cors = require('cors')
+
+var match = require('./matchOpponent');
+var util = require('./util');
+
+http.listen('8080', () => {
+    console.log("listening 8080.........")
+})
+
+app.use(cors());
+
+//路由部分
+var router = require('./router/index');
+app.use(router);
+
+
+
+// 每个用户socketId
+var socketList = {};
+// 正在请求匹配的集合
+var matching = [];
+// 在线用户列表
+let onlineUser = [];
+
+io.on('connection', (socket) => {
+
+    // 保存每一个登录用户的socket及相关状态信息
+    socketList[socket.id] = {
+        opponent: null,         // 对手socketid
+        status: 0,              // 状态: 0, 刚进入; 1: 请求匹配中; 2: 游戏中;
+        name: ''                // 该用户的名称
+    }
+
+    // 第一步首先获取 新用户的昵称; 格式 {userName: "Tom"} 
+    socket.on('newUserName', (msg) => {
+
+        console.log("newUser coming --- " + msg.userName + "\n");
+        console.log(`当前在线用户共 ${Object.keys(socketList).length} 人\n\n`);
+        onlineUser.push(
+            {
+                name: msg.userName,
+                id: socket.id,
+                status: 0,
+            }
+        );
+        // 将用户名称存入在线玩家列表
+        socketList[socket.id].name = msg.userName;
+        // onlineUser 数组中id相同的用户去重
+        // 将用户名称返回在线玩家列表
+        socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        // 通知所有在线用户更新在线用户列表
+        io.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+    })
+    socket.on('listOnlineUserResponse', (msg) => {
+        console.log('listOnlineUserResponse', msg);
+    })
+    // 用户点击 开始游戏, 触发对手匹配
+    socket.on('startGame', startGameCallBack);
+
+    function startGameCallBack(msg) {
+
+        console.log(`用户${msg.userName}开始匹配游戏....`);
+        console.log(msg);
+
+        // 将其加入请求匹配的数组中
+        matching.push(socket.id);
+        // 修改该socket状态
+        socketList[socket.id].status = 1;
+
+        var deal = match.doMatch(matching, socket, socketList);
+        // 修改start状态
+        onlineUser = onlineUser.map((item) => {
+            if (item.id == socket.id) {
+                item.status = 1;
+            }
+            return item;
+        })
+        socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        io.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        deal.then((result) => {
+            // 将两socket状态改为gaming状态 (2)
+            socketList[socket.id].status = 2;
+            socketList[result].status = 2;
+            // 修改onlineUser状态
+            onlineUser = onlineUser.map((item) => {
+                if (item.id == socket.id || item.id == result) {
+                    item.status = 2;
+                }
+                return item;
+            })
+            socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+            io.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+            // 标记对手
+            socketList[socket.id].opponent = result;
+            socketList[result].opponent = socket.id;
+
+            // 将两个socket从请求匹配的数组中移除
+            matching = matching.filter((item) => {
+                return item != socket.id && item != result;
+            })
+
+            var myTurn = socket.id > result;
+            // 告诉双方匹配成功
+            socket.to(result).emit('startGameResponse', { status: 0, againstId: socket.id, againstName: socketList[socket.id].name, myTurn: myTurn });
+            //socket.emit('startGameResponse', {status: 0, againstId: result, againstName: socketList[result].name, myTurn: true});
+
+            console.log(`用户 ${socketList[socket.id].name} 和 用户 ${socketList[result].name} 游戏匹配成功....\n`);
+        }).catch(() => {
+            if (socketList[socket.id].status == 1) {
+                socket.emit('startGameResponse', { status: 1 });              // 匹配失败, 告诉发起方, 匹配失败信息;
+                matching = matching.filter((item) => {
+                    return item != socket.id;
+                })
+            }
+            // 修改onlineUser状态
+            onlineUser = onlineUser.map((item) => {
+                if (item.id == socket.id) {
+                    item.status = 0;
+                }
+                return item;
+            })
+            socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+            io.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+            console.log(`用户 ${socketList[socket.id].name} 尝试匹配 失败 !!!! \n`);
+        })
+    }
+
+    // 下棋     收到信息格式 {isWin: false, coordinate: [3, 4], againstId: socketid, myTurn: true}
+    socket.on('chess', (msg) => {
+        console.log(msg.coordinate);
+
+        if (!msg.isWin) {
+            if (msg.myTurn) {
+                socket.to(msg.againstId).emit('chessResponse', { againstId: msg.againstId, myTurn: true, isWin: false, isLose: false, coordinate: msg.coordinate });
+                socket.emit('changeTurn', { myTurn: false });
+                // 修改onlineUser状态
+                onlineUser = onlineUser.map((item) => {
+                    if (item.id == socket.id) {
+                        item.status = 0;
+                    }
+                    return item;
+                }
+                )
+                socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+            }
+        } else {
+            // 通知输掉的一方
+            socket.to(msg.againstId).emit('chessResponse', { againstId: msg.againstId, myTurn: false, isWin: false, isLose: true, coordinate: msg.coordinate });
+            socket.emit('changeTurn', { myTurn: false })
+
+            // 显示该回合结束信息
+            console.log(`${socketList[msg.againstId].name} 和　${socketList[socket.id]}　本回合结束，　${socketList[socket.id]} 胜利！\n`);
+
+            // 重置双方的游戏状态
+            util.resetStatus(socketList, socket.id, msg.againstId);
+            // 修改onlineUser状态
+            onlineUser = onlineUser.map((item) => {
+                if (item.id == socket.id || item.id == msg.againstId) {
+                    item.status = 0;
+                }
+                return item;
+            })
+            socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+            io.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        }
+    })
+
+    // 发送聊天消息     收到发消息的格式 {againstId: againstId, msg: str}
+    socket.on('sendMsg', (msg) => {
+        socket.to(msg.againstId).emit('recvMsg', { msg: msg.msg });
+    })
+
+    // 监听客户端发来的意外消息     accident事件, status: 0, 对方掉线; 1, 对方认输, 2, 对方申请悔棋;
+    socket.on('accident', (msg) => {
+
+        if (msg.status == 1) {        // 表示发送该消息方 认输
+            socket.to(socketList[socket.id].opponent).emit('accident', { status: 1 });
+            // 重置双方的游戏状态
+            util.resetStatus(socketList, socket.id, socketList[socket.id].opponent);
+            // 修改onlineUser状态
+            onlineUser = onlineUser.map((item) => {
+                if (item.id == socket.id || item.id == socketList[socket.id].opponent) {
+                    item.status = 0;
+                }
+                return item;
+            }
+            )
+            socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        }
+        if (msg.status == 0) {
+            socket.to(socketList[socket.id].opponent).emit('accident', { status: 0 });
+            // 重置双方的游戏状态
+            util.resetStatus(socketList, socket.id, socketList[socket.id].opponent);
+            // 修改onlineUser状态
+            onlineUser = onlineUser.map((item) => {
+                if (item.id == socket.id || item.id == socketList[socket.id].opponent) {
+                    item.status = 0;
+                }
+                return item;
+            }
+            )
+            socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        }
+        if (msg.status == 2) {
+            socket.to(socketList[socket.id].opponent).emit('accidentClient', { status: 2 });
+            // 修改onlineUser状态
+            onlineUser = onlineUser.map((item) => {
+                if (item.id == socket.id || item.id == socketList[socket.id].opponent) {
+                    item.status = 0;
+                }
+                return item;
+            }
+            )
+            socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        }
+        // 修改onlineUser状态
+        onlineUser = onlineUser.map((item) => {
+            if (item.id == socket.id || item.id == socketList[socket.id].opponent) {
+                item.status = 0;
+            }
+            return item;
+        }
+        )
+        socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+    })
+
+    socket.on('repentRespose', (msg) => { // msg格式　｛ isAgree: false }
+        socket.to(socketList[socket.id].opponent).emit('reciveRepentResult', {
+            isAgree: msg.isAgree
+        });
+    })
+
+    // 客户端断开连接
+    socket.on('disconnect', () => {
+
+        console.log(`用户 ${socketList[socket.id].name} 断开连接....\n`);
+        // 如果该断开用户处于匹配状态中
+        if (socketList[socket.id].status == 1) {
+            matching = matching.filter((item) => {
+                return item != socket.id;
+            })
+        }
+
+        // 如果断开用户处于游戏状态中
+        if (socketList[socket.id].status == 2) {             // status: 0 表示对方掉线
+            socket.to(socketList[socket.id].opponent).emit('accident', { status: 0 });
+
+            // 重置双方的游戏状态
+            util.resetStatus(socketList, socket.id, socketList[socket.id].opponent);
+        }
+
+        delete socketList[socket.id];
+        onlineUser = onlineUser.filter((item) => {
+            return item.id != socket.id;
+        })
+        socket.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        io.emit('listOnlineUserResponse', { status: 0, onlineUser: onlineUser });
+        console.log(`当前在线用户共 ${Object.keys(socketList).length} 人\n\n`);
+    })
+})
---
+++ util.js
@@ -0,0 +1,12 @@
+// 每回合结束后, 将双方的状态/对手清空
+exports.resetStatus = function (socketList, sa, sb) {
+    try {
+        socketList[sa].status = 0;
+        socketList[sb].status = 0;
+        socketList[sa].opponent = null;
+        socketList[sb].opponent = null;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
